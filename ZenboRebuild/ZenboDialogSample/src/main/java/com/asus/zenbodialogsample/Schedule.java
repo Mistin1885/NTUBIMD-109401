80d8c6d307e7dbdf05554972ae79caec9d8fd23f
Mistin <gcp940651@gmail.com>
2020-11-22 18:43:34 +0800

    Upload ZenboApp_androidx v0.5

---
+++ ZenboRebuild/ZenboDialogSample/src/main/java/com/asus/zenbodialogsample/Schedule.java
@@ -0,0 +1,40 @@
+package com.asus.zenbodialogsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class Schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone("Asia/Taipei"));
+
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1 ;
+        if(w<0){
+            w = 0;
+        }
+
+        ImageView image_week = findViewById(R.id.schedule_mon);
+
+        if(weekDays[w] == "星期五"){
+            image_week = (ImageView) findViewById(R.id.schedule_fri);
+        }
+
+        image_week.setVisibility(View.VISIBLE);
+    }
+
+    //返回按鈕
+    public void goBack(View v){
+        finish();
+    }
+}
